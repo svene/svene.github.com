145d0fd4f1ef8f7de7017bb96f4fbdce1c299e59
Sven Ehrke <sven.ehrke@canoo.com>
2011-10-16 19:23:33 +0200

    changed mvn repo url to svene.github.com

--- jdt.groovy
+++ jdt.groovy
@@ -1,4 +1,5 @@
-@GrabResolver(name='sven-local', root='file://localhost/home/sven/se/sweng/_github/svene/svene.github.com/maven2/releases')
+//@GrabResolver(name='sven-local', root='file://localhost/home/sven/se/sweng/_github/svene/svene.github.com/maven2/releases')
+@GrabResolver(name='sven-github', root='http://svene.github.com/maven2/releases')
 @Grapes([
 	@Grab(group='org.svenehrke', module='dt_java', version='0.0.1')
 ])
